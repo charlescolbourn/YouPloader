6cbdeacef85ad248157b13a6a5f752403c13e6f0
genuineparts <bernhard@becast.at>
2016-04-06 22:17:04 +0200

    Gui Work

--- src/main/java/at/becast/youploader/gui/PlaylistPanel.java
+++ src/main/java/at/becast/youploader/gui/PlaylistPanel.java
@@ -20,6 +20,9 @@ import javax.swing.SwingConstants;
 
 import javax.swing.SpringLayout;
 import java.awt.Font;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
 
 public class PlaylistPanel extends JPanel {
 
@@ -32,21 +35,12 @@ public class PlaylistPanel extends JPanel {
 	 * Create the panel.
 	 */
 	public PlaylistPanel() {
+		setLayout(new FormLayout(new ColumnSpec[] {},
+			new RowSpec[] {}));
 		initComponents();
 	}
 	
 	private void initComponents(){
-		SpringLayout springLayout = new SpringLayout();
-		setLayout(springLayout);
-		
-		JLabel lblNotImplementedYet = new JLabel("Not implemented yet :( Sorry!");
-		springLayout.putConstraint(SpringLayout.NORTH, lblNotImplementedYet, 131, SpringLayout.NORTH, this);
-		springLayout.putConstraint(SpringLayout.WEST, lblNotImplementedYet, 32, SpringLayout.WEST, this);
-		springLayout.putConstraint(SpringLayout.SOUTH, lblNotImplementedYet, -147, SpringLayout.SOUTH, this);
-		springLayout.putConstraint(SpringLayout.EAST, lblNotImplementedYet, -34, SpringLayout.EAST, this);
-		lblNotImplementedYet.setFont(new Font("Tahoma", Font.BOLD, 18));
-		lblNotImplementedYet.setHorizontalAlignment(SwingConstants.CENTER);
-		add(lblNotImplementedYet);
 	}
 
 }
