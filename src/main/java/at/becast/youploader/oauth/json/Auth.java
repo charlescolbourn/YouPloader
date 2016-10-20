e8e594d8069d6e6c1a4fb9c0e4a1ab1c436a7ed1
genuineparts <bernhard@becast.at>
2016-10-20 19:29:23 +0200

    Fixes login issue. Fixing #35

--- src/main/java/at/becast/youploader/oauth/json/Auth.java
+++ src/main/java/at/becast/youploader/oauth/json/Auth.java
@@ -3,21 +3,23 @@ package at.becast.youploader.oauth.json;
 public class Auth {
   public final String error;
   public final String error_description;
+  public final String error_uri;
   public final String access_token;
   public final String token_type;
   public final int expires_in;
   public final String refresh_token;
 
-  public Auth(String error, String error_description, String access_token, String token_type, int expires_in, String refresh_token) {
+  public Auth(String error, String error_description, String access_token, String token_type, int expires_in, String refresh_token, String error_uri) {
     this.error = error;
     this.error_description = error_description;
     this.access_token = access_token;
     this.token_type = token_type;
     this.expires_in = expires_in;
     this.refresh_token = refresh_token;
+    this.error_uri = error_uri;
   }
 
   public Auth() {
-    this(null, null, null, null, 0, null);
+    this(null, null, null, null, 0, null, null);
   }
 }
