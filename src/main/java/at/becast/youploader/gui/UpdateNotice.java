75a565fb7586b6c6ad51135ebf34162fc8f18061
genuineparts <bernhard@becast.at>
2017-03-18 13:18:11 +0100

    File separator platform independence

--- src/main/java/at/becast/youploader/gui/UpdateNotice.java
+++ src/main/java/at/becast/youploader/gui/UpdateNotice.java
@@ -1,3 +1,17 @@
+/* 
+ * YouPloader Copyright (c) 2017 genuineparts (itsme@genuineparts.org)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ */
 package at.becast.youploader.gui;
 
 import java.awt.BorderLayout;
@@ -111,8 +125,8 @@ public class UpdateNotice extends JDialog implements ActionListener {
 							   @Override
 							   protected void done() {
 								   Unzip z = new Unzip();
-								   z.unZipIt(System.getProperty("user.dir") + "/update.zip", System.getProperty("user.dir") + "/update/");
-								   new File(System.getProperty("user.dir") + "/update.zip").delete();
+								   z.unZipIt(System.getProperty("user.dir") + File.separator + "update.zip", System.getProperty("user.dir") + File.separator + "update" + File.separator);
+								   new File(System.getProperty("user.dir") + File.separator + "update.zip").delete();
 							   }
 							  };
 							  
@@ -166,8 +180,8 @@ public class UpdateNotice extends JDialog implements ActionListener {
         OutputStream os = null;
         InputStream is = null;
         try {
-        	new File(System.getProperty("user.dir") + "/update/").mkdirs();
-            fl = new File(System.getProperty("user.dir") + "/update.zip");
+        	new File(System.getProperty("user.dir") + File.separator + "update" + File.separator).mkdirs();
+            fl = new File(System.getProperty("user.dir") + File.separator +"update.zip");
             dl = new URL("https://version.youploader.com/updates/" + this.updatefile);
             os = new FileOutputStream(fl);
             is = dl.openStream();
